a5164484e74df8395a9e925fbddafd43fca0a0ab
Michell Cuni <cc.eduardo88@gmail.com>
2024-06-28 00:22:16 -0600

    edit example2

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const fn = () => {
     const miString = "soy un string!"
     let b = miString + " muy feliz"
     console.log(b)
+    console.log('Hola mundo');        
 }
 
 fn()
 
 const clickeado = () => {
     let a = "chanchito feliz"
-
     console.log(a)
 }
